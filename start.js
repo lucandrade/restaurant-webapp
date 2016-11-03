5850c4bbf3ed0475d2fab79bb76de6052fd8ff99
Lucas Andrade <lucas.andrade.oliveira@hotmail.com>
2016-11-03 00:19:55 -0200

    adjusting production variables

--- start.js
+++ start.js
@@ -5,9 +5,11 @@ var serverStatic = require('serve-static');
 var server = connect();
 var livereload = require('livereload');
 var port = process.env.PORT || 8080;
+var production = process.env.PROD || false;
+var folder = production ? '/dist' : '/server';
 
-server.use(serverStatic(__dirname + '/server'));
+server.use(serverStatic(__dirname + folder));
 server.listen(port);
 server = livereload.createServer();
-server.watch(__dirname + "/server");
+server.watch(__dirname + folder);
 console.log('The app is running in http://localhost:' + port);
